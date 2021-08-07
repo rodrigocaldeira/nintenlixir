80ef0e8cfd09c92b119d79321a36b5390544f74c
Rodrigo Caldeira <rodrigocaldeira@gmail.com>
2021-08-07 19:13:08 -0300

    Refactoring and some flags and addressing resolving

--- lib/nintenlixir/cpu.ex
+++ lib/nintenlixir/cpu.ex
@@ -48,13 +48,13 @@ defmodule Nintenlixir.CPU do
         _ -> 7
       end
 
-    %{processor_status: p} = Registers.get_registers(registers_server_name())
+    %{processor_status: p} = get_registers()
 
     if irq && (p &&& ProcessorStatus.InterruptDisable.value()) == 0 do
       %{
         program_counter: pc,
         processor_status: p
-      } = Registers.get_registers(registers_server_name())
+      } = get_registers()
 
       :ok = push16(pc)
 
@@ -66,15 +66,15 @@ defmodule Nintenlixir.CPU do
 
       p = p ||| ProcessorStatus.InterruptDisable.value()
 
-      {:ok, low} = Memory.read(memory_server_name(), 0xFFFE)
-      {:ok, high} = Memory.read(memory_server_name(), 0xFFFF)
+      {:ok, low} = read_memory(0xFFFE)
+      {:ok, high} = read_memory(0xFFFF)
 
       pc = high <<< 8 ||| low
 
-      registers = Registers.get_registers(registers_server_name())
+      registers = get_registers()
 
       :ok =
-        Registers.set_registers(registers_server_name(), %{
+        set_registers(%{
           registers
           | program_counter: pc,
             processor_status: p
@@ -87,7 +87,7 @@ defmodule Nintenlixir.CPU do
       %{
         program_counter: pc,
         processor_status: p
-      } = Registers.get_registers(registers_server_name())
+      } = get_registers()
 
       :ok = push16(pc)
 
@@ -99,15 +99,15 @@ defmodule Nintenlixir.CPU do
 
       p = p ||| ProcessorStatus.InterruptDisable.value()
 
-      {:ok, low} = Memory.read(memory_server_name(), 0xFFFA)
-      {:ok, high} = Memory.read(memory_server_name(), 0xFFFB)
+      {:ok, low} = read_memory(0xFFFA)
+      {:ok, high} = read_memory(0xFFFB)
 
       pc = high <<< 8 ||| low
 
-      registers = Registers.get_registers(registers_server_name())
+      registers = get_registers()
 
       :ok =
-        Registers.set_registers(registers_server_name(), %{
+        set_registers(%{
           registers
           | program_counter: pc,
             processor_status: p
@@ -124,6 +124,111 @@ defmodule Nintenlixir.CPU do
     {:ok, cycles}
   end
 
+  def set_Z_flag(0x00) do
+    %{processor_status: p} = registers = get_registers()
+
+    :ok =
+      set_registers(%{
+        registers
+        | processor_status: p ||| ProcessorStatus.ZeroFlag.value()
+      })
+
+    {:ok, 0x00}
+  end
+
+  def set_Z_flag(value) do
+    %{processor_status: p} = registers = get_registers()
+
+    :ok =
+      set_registers(%{
+        registers
+        | processor_status: p &&& ~~~ProcessorStatus.ZeroFlag.value()
+      })
+
+    {:ok, value}
+  end
+
+  def set_N_flag(value) do
+    %{processor_status: p} = registers = get_registers()
+
+    :ok =
+      set_registers(%{
+        registers
+        | processor_status:
+            (p &&& ~~~ProcessorStatus.NegativeFlag.value()) |||
+              (value &&& ProcessorStatus.NegativeFlag.value())
+      })
+
+    {:ok, value}
+  end
+
+  def set_C_flag_addition(value) do
+    %{processor_status: p} = registers = get_registers()
+
+    :ok =
+      set_registers(%{
+        registers
+        | processor_status:
+            (p &&& ~~~ProcessorStatus.CarryFlag.value()) |||
+              (value >>> 8 &&& ProcessorStatus.CarryFlag.value())
+      })
+
+    {:ok, value}
+  end
+
+  def set_V_flag_addition(term1, term2, result) do
+    %{processor_status: p} = registers = get_registers()
+
+    :ok =
+      set_registers(%{
+        registers
+        | processor_status:
+            (p &&& ~~~ProcessorStatus.OverflowFlag.value()) |||
+              (~~~(bxor(term1, term2) &&& bxor(term1, result)) &&&
+                 ProcessorStatus.NegativeFlag.value()) >>> 1
+      })
+
+    {:ok, result}
+  end
+
+  def immediate_address do
+    %{program_counter: pc} = registers = get_registers()
+    :ok = set_registers(%{registers | program_counter: pc + 1})
+    {:ok, pc}
+  end
+
+  def zero_page_address do
+    %{program_counter: pc} = registers = get_registers()
+    {:ok, result} = read_memory(pc)
+    :ok = set_registers(%{registers | program_counter: pc + 1})
+    {:ok, result}
+  end
+
+  def relative_address do
+    %{program_counter: pc} = registers = get_registers()
+    {:ok, value} = read_memory(pc)
+    pc = pc + 1
+    :ok = set_registers(%{registers | program_counter: pc})
+
+    offset =
+      if value > 0x7F do
+        -(0x0100 - value)
+      else
+        value
+      end
+
+    {:ok, pc + offset &&& 0xFFFF}
+  end
+
+  def absolute_address do
+    %{program_counter: pc} = registers = get_registers()
+    {:ok, low} = read_memory(pc)
+    {:ok, high} = read_memory(pc + 1)
+    :ok = set_registers(%{registers | program_counter: pc + 2})
+
+    {:ok, high <<< 8 ||| low}
+  end
+
   # Server
 
   @impl GenServer
@@ -137,36 +242,36 @@ defmodule Nintenlixir.CPU do
   end
 
   def handle_call(:reset, _, state) do
-    {:ok, low} = Memory.read(memory_server_name(), 0xFFFC)
-    {:ok, high} = Memory.read(memory_server_name(), 0xFFFD)
+    {:ok, low} = read_memory(0xFFFC)
+    {:ok, high} = read_memory(0xFFFD)
 
     pc = high <<< 8 ||| low
 
-    registers = Registers.get_registers(registers_server_name())
+    registers = get_registers()
 
-    :ok = Registers.set_registers(registers_server_name(), %{registers | program_counter: pc})
+    :ok = set_registers(%{registers | program_counter: pc})
 
     {:reply, :ok, state}
   end
 
   def handle_call({:push, value}, _, state) do
-    %{stack_pointer: sp} = registers = Registers.get_registers(registers_server_name())
+    %{stack_pointer: sp} = registers = get_registers()
 
-    :ok = Memory.write(memory_server_name(), 0x0100 ||| sp, value)
+    :ok = write_memory(0x0100 ||| sp, value)
 
-    :ok = Registers.set_registers(registers_server_name(), %{registers | stack_pointer: sp - 1})
+    :ok = set_registers(%{registers | stack_pointer: sp - 1})
 
     {:reply, :ok, state}
   end
 
   def handle_call(:pop, _, state) do
-    %{stack_pointer: sp} = registers = Registers.get_registers(registers_server_name())
+    %{stack_pointer: sp} = registers = get_registers()
 
     sp = sp + 1
 
-    {:ok, value} = Memory.read(memory_server_name(), 0x0100 ||| sp)
+    {:ok, value} = read_memory(0x0100 ||| sp)
 
-    :ok = Registers.set_registers(registers_server_name(), %{registers | stack_pointer: sp})
+    :ok = set_registers(%{registers | stack_pointer: sp})
 
     {:reply, {:ok, value}, state}
   end
@@ -190,4 +295,10 @@ defmodule Nintenlixir.CPU do
 
   def memory_server_name, do: :memory_cpu
   def registers_server_name, do: :registers_cpu
+
+  defp get_registers, do: Registers.get_registers(registers_server_name())
+  defp set_registers(registers), do: Registers.set_registers(registers_server_name(), registers)
+
+  defp read_memory(address), do: Memory.read(memory_server_name(), address)
+  defp write_memory(address, value), do: Memory.write(memory_server_name(), address, value)
 end
--- test/cpu_test.exs
+++ test/cpu_test.exs
@@ -31,53 +31,53 @@ defmodule Nintenlixir.CPUTest do
   end
 
   test "CPU.push/1" do
-    %{stack_pointer: sp} = Registers.get_registers(CPU.registers_server_name())
+    %{stack_pointer: sp} = get_registers()
 
     assert :ok = CPU.push(0xCA)
 
-    %{stack_pointer: new_sp} = Registers.get_registers(CPU.registers_server_name())
+    %{stack_pointer: new_sp} = get_registers()
 
     assert new_sp == sp - 1
 
-    assert {:ok, 0xCA} = Memory.read(CPU.memory_server_name(), 0x0100 ||| sp)
+    assert {:ok, 0xCA} = read_memory(0x0100 ||| sp)
   end
 
   test "CPU.push16/1" do
-    %{stack_pointer: sp} = Registers.get_registers(CPU.registers_server_name())
+    %{stack_pointer: sp} = get_registers()
 
     assert :ok = CPU.push16(0xCAFE)
 
-    %{stack_pointer: new_sp} = Registers.get_registers(CPU.registers_server_name())
+    %{stack_pointer: new_sp} = get_registers()
 
     assert new_sp == sp - 2
 
-    assert {:ok, 0xCA} = Memory.read(CPU.memory_server_name(), 0x0100 ||| sp)
+    assert {:ok, 0xCA} = read_memory(0x0100 ||| sp)
 
-    assert {:ok, 0xFE} = Memory.read(CPU.memory_server_name(), 0x0100 ||| sp - 1)
+    assert {:ok, 0xFE} = read_memory(0x0100 ||| sp - 1)
   end
 
   test "CPU.pop/0" do
-    %{stack_pointer: sp} = Registers.get_registers(CPU.registers_server_name())
+    %{stack_pointer: sp} = get_registers()
 
-    Memory.write(CPU.memory_server_name(), 0x0100 ||| sp + 1, 0xCA)
+    write_memory(0x0100 ||| sp + 1, 0xCA)
 
     assert {:ok, 0xCA} = CPU.pop()
 
-    %{stack_pointer: new_sp} = Registers.get_registers(CPU.registers_server_name())
+    %{stack_pointer: new_sp} = get_registers()
 
     assert new_sp == sp + 1
   end
 
   test "CPU.pop16/0" do
-    %{stack_pointer: sp} = Registers.get_registers(CPU.registers_server_name())
+    %{stack_pointer: sp} = get_registers()
 
-    Memory.write(CPU.memory_server_name(), 0x0100 ||| sp + 2, 0xCA)
+    write_memory(0x0100 ||| sp + 2, 0xCA)
 
-    Memory.write(CPU.memory_server_name(), 0x0100 ||| sp + 1, 0xFE)
+    write_memory(0x0100 ||| sp + 1, 0xFE)
 
     assert {:ok, 0xCAFE} = CPU.pop16()
 
-    %{stack_pointer: new_sp} = Registers.get_registers(CPU.registers_server_name())
+    %{stack_pointer: new_sp} = get_registers()
 
     assert new_sp == sp + 2
   end
@@ -102,10 +102,10 @@ defmodule Nintenlixir.CPUTest do
   end
 
   test "CPU.interrupt/0 with irq" do
-    %{processor_status: p} = registers = Registers.get_registers(CPU.registers_server_name())
+    %{processor_status: p} = registers = get_registers()
 
     assert :ok =
-             Registers.set_registers(CPU.registers_server_name(), %{
+             set_registers(%{
                registers
                | processor_status: p &&& ~~~ProcessorStatus.InterruptDisable.value()
              })
@@ -114,10 +114,10 @@ defmodule Nintenlixir.CPUTest do
       program_counter: pc,
       processor_status: p,
       stack_pointer: sp
-    } = Registers.get_registers(CPU.registers_server_name())
+    } = get_registers()
 
-    {:ok, new_pc_low} = Memory.read(CPU.memory_server_name(), 0xFFFE)
-    {:ok, new_pc_high} = Memory.read(CPU.memory_server_name(), 0xFFFF)
+    {:ok, new_pc_low} = read_memory(0xFFFE)
+    {:ok, new_pc_high} = read_memory(0xFFFF)
 
     assert :ok = CPU.irq()
     assert %{irq: true} = CPU.get_state()
@@ -128,11 +128,11 @@ defmodule Nintenlixir.CPUTest do
       program_counter: new_pc,
       processor_status: new_p,
       stack_pointer: new_sp
-    } = Registers.get_registers(CPU.registers_server_name())
+    } = get_registers()
 
-    {:ok, previous_p} = Memory.read(CPU.memory_server_name(), 0x0100 ||| sp - 2)
-    {:ok, previous_pc_low} = Memory.read(CPU.memory_server_name(), 0x0100 ||| sp - 1)
-    {:ok, previous_pc_high} = Memory.read(CPU.memory_server_name(), 0x0100 ||| sp)
+    {:ok, previous_p} = read_memory(0x0100 ||| sp - 2)
+    {:ok, previous_pc_low} = read_memory(0x0100 ||| sp - 1)
+    {:ok, previous_pc_high} = read_memory(0x0100 ||| sp)
 
     assert ((previous_p ||| ProcessorStatus.Unused.value()) &&&
               ~~~ProcessorStatus.BreakCommand.value()) == p
@@ -148,10 +148,10 @@ defmodule Nintenlixir.CPUTest do
       program_counter: pc,
       processor_status: p,
       stack_pointer: sp
-    } = Registers.get_registers(CPU.registers_server_name())
+    } = get_registers()
 
-    {:ok, new_pc_low} = Memory.read(CPU.memory_server_name(), 0xFFFA)
-    {:ok, new_pc_high} = Memory.read(CPU.memory_server_name(), 0xFFFB)
+    {:ok, new_pc_low} = read_memory(0xFFFA)
+    {:ok, new_pc_high} = read_memory(0xFFFB)
 
     assert :ok = CPU.nmi()
     assert %{nmi: true} = CPU.get_state()
@@ -162,11 +162,11 @@ defmodule Nintenlixir.CPUTest do
       program_counter: new_pc,
       processor_status: new_p,
       stack_pointer: new_sp
-    } = Registers.get_registers(CPU.registers_server_name())
+    } = get_registers()
 
-    {:ok, previous_p} = Memory.read(CPU.memory_server_name(), 0x0100 ||| sp - 2)
-    {:ok, previous_pc_low} = Memory.read(CPU.memory_server_name(), 0x0100 ||| sp - 1)
-    {:ok, previous_pc_high} = Memory.read(CPU.memory_server_name(), 0x0100 ||| sp)
+    {:ok, previous_p} = read_memory(0x0100 ||| sp - 2)
+    {:ok, previous_pc_low} = read_memory(0x0100 ||| sp - 1)
+    {:ok, previous_pc_high} = read_memory(0x0100 ||| sp)
 
     assert ((previous_p ||| ProcessorStatus.Unused.value()) &&&
               ~~~ProcessorStatus.BreakCommand.value()) == p
@@ -182,6 +182,72 @@ defmodule Nintenlixir.CPUTest do
     assert %{rst: true} = CPU.get_state()
     assert {:ok, 7} = CPU.interrupt()
     assert %{rst: false} = CPU.get_state()
-    assert %{program_counter: 0xFFFF} = Registers.get_registers(CPU.registers_server_name())
+    assert %{program_counter: 0xFFFF} = get_registers()
   end
+
+  test "CPU.set_Z_flag/1 with 0x00" do
+    assert {:ok, 0x00} = CPU.set_Z_flag(0x00)
+    assert %{processor_status: 0x26} = get_registers()
+  end
+
+  test "CPU.set_Z_flag/1 with other value" do
+    assert {:ok, 0xCA} = CPU.set_Z_flag(0xCA)
+    assert %{processor_status: 0x24} = get_registers()
+  end
+
+  test "CPU.set_N_flag/1" do
+    assert {:ok, 0xCA} = CPU.set_N_flag(0xCA)
+    assert %{processor_status: 0xA4} = get_registers()
+  end
+
+  test "CPU.set_C_flag_addition/1" do
+    assert {:ok, 0xCA} = CPU.set_C_flag_addition(0xCA)
+    assert %{processor_status: 0x24} = get_registers()
+  end
+
+  test "CPU.set_V_flag_addition/3" do
+    assert {:ok, 0x80} = CPU.set_V_flag_addition(0x78, 0x08, 0x80)
+    assert %{processor_status: 0x64} = get_registers()
+  end
+
+  test "CPU.immediate_address/0" do
+    assert {:ok, 0xFFFC} = CPU.immediate_address()
+    assert %{program_counter: 0xFFFD} = get_registers()
+  end
+
+  test "CPU.zero_page_address/0" do
+    {:ok, 0xFF} = CPU.zero_page_address()
+    assert %{program_counter: 0xFFFD} = get_registers()
+  end
+
+  test "CPU.relative_address/0 when value in memory is bigger than 0x7F" do
+    %{program_counter: pc} = get_registers()
+    write_memory(pc, 0x80)
+    assert {:ok, 0xFF7D} = CPU.relative_address()
+    assert %{program_counter: 0xFFFD} = get_registers()
+  end
+
+  test "CPU.relative_address/0 when value in memory is lesser or equal to 0x7F" do
+    %{program_counter: pc} = get_registers()
+    write_memory(pc, 0x7F)
+    assert {:ok, 0x7C} = CPU.relative_address()
+    assert %{program_counter: 0xFFFD} = get_registers()
+  end
+
+  test "CPU.absolute_address/0" do
+    %{program_counter: pc} = get_registers()
+    write_memory(pc, 0xFE)
+    write_memory(pc + 1, 0xCA)
+    assert {:ok, 0xCAFE} = CPU.absolute_address()
+    assert %{program_counter: 0xFFFE} = get_registers()
+  end
+
+  # Helpers
+  def get_registers, do: Registers.get_registers(CPU.registers_server_name())
+
+  def set_registers(registers),
+    do: Registers.set_registers(CPU.registers_server_name(), registers)
+
+  def read_memory(address), do: Memory.read(CPU.memory_server_name(), address)
+  def write_memory(address, value), do: Memory.write(CPU.memory_server_name(), address, value)
 end
