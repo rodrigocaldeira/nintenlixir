615e9c3404d29a3556f8761a5cabe5bd733d8c01
Rodrigo Caldeira <rodrigocaldeira@gmail.com>
2021-08-07 11:37:06 -0300

    Interrupt

--- lib/nintenlixir/cpu.ex
+++ lib/nintenlixir/cpu.ex
@@ -6,14 +6,13 @@ defmodule Nintenlixir.CPU do
 
   alias Nintenlixir.Memory
   alias Nintenlixir.Registers
+  alias Nintenlixir.ProcessorStatus
 
   def start_link(_) do
     GenServer.start_link(__MODULE__, new_cpu(), name: __MODULE__)
   end
 
-  def get_state do
-    GenServer.call(__MODULE__, :get_state)
-  end
+  def get_state, do: GenServer.call(__MODULE__, :get_state)
 
   def reset do
     :ok = Memory.reset(memory_server_name())
@@ -21,24 +20,108 @@ defmodule Nintenlixir.CPU do
     GenServer.call(__MODULE__, :reset)
   end
 
-  def push(value) do
-    GenServer.call(__MODULE__, {:push, value})
-  end
+  def push(value), do: GenServer.call(__MODULE__, {:push, value})
 
   def push16(value) do
     :ok = push(value >>> 8)
     push(value &&& 0x00FF)
   end
 
-  def pop do
-    GenServer.call(__MODULE__, :pop)
-  end
+  def pop, do: GenServer.call(__MODULE__, :pop)
 
   def pop16 do
     {:ok, low} = pop()
     {:ok, high} = pop()
-
     {:ok, high <<< 8 ||| low}
+  end
+
+  def irq, do: GenServer.call(__MODULE__, {:receive_interrupt, :irq})
+  def nmi, do: GenServer.call(__MODULE__, {:receive_interrupt, :nmi})
+  def rst, do: GenServer.call(__MODULE__, {:receive_interrupt, :rst})
+
+  def interrupt do
+    %{irq: irq, nmi: nmi, rst: rst} = state = GenServer.call(__MODULE__, :get_state)
+
+    cycles =
+      case [irq, nmi, rst] do
+        [false, false, false] -> 0
+        _ -> 7
+      end
+
+    %{processor_status: p} = Registers.get_registers(registers_server_name())
+
+    if irq && (p &&& ProcessorStatus.InterruptDisable.value()) == 0 do
+      %{
+        program_counter: pc,
+        processor_status: p
+      } = Registers.get_registers(registers_server_name())
+
+      :ok = push16(pc)
+
+      :ok =
+        push(
+          (p ||| ProcessorStatus.Unused.value()) &&&
+            ~~~ProcessorStatus.BreakCommand.value()
+        )
+
+      p = p ||| ProcessorStatus.InterruptDisable.value()
+
+      {:ok, low} = Memory.read(memory_server_name(), 0xFFFE)
+      {:ok, high} = Memory.read(memory_server_name(), 0xFFFF)
+
+      pc = high <<< 8 ||| low
+
+      registers = Registers.get_registers(registers_server_name())
+
+      :ok =
+        Registers.set_registers(registers_server_name(), %{
+          registers
+          | program_counter: pc,
+            processor_status: p
+        })
+
+      :ok = GenServer.call(__MODULE__, {:set_state, %{state | irq: false}})
+    end
+
+    if nmi do
+      %{
+        program_counter: pc,
+        processor_status: p
+      } = Registers.get_registers(registers_server_name())
+
+      :ok = push16(pc)
+
+      :ok =
+        push(
+          (p ||| ProcessorStatus.Unused.value()) &&&
+            ~~~ProcessorStatus.BreakCommand.value()
+        )
+
+      p = p ||| ProcessorStatus.InterruptDisable.value()
+
+      {:ok, low} = Memory.read(memory_server_name(), 0xFFFA)
+      {:ok, high} = Memory.read(memory_server_name(), 0xFFFB)
+
+      pc = high <<< 8 ||| low
+
+      registers = Registers.get_registers(registers_server_name())
+
+      :ok =
+        Registers.set_registers(registers_server_name(), %{
+          registers
+          | program_counter: pc,
+            processor_status: p
+        })
+
+      :ok = GenServer.call(__MODULE__, {:set_state, %{state | nmi: false}})
+    end
+
+    if rst do
+      :ok = GenServer.call(__MODULE__, :reset)
+      :ok = GenServer.call(__MODULE__, {:set_state, %{state | rst: false}})
+    end
+
+    {:ok, cycles}
   end
 
   # Server
@@ -87,6 +170,12 @@ defmodule Nintenlixir.CPU do
 
     {:reply, {:ok, value}, state}
   end
+
+  def handle_call({:receive_interrupt, :irq}, _, state), do: {:reply, :ok, %{state | irq: true}}
+  def handle_call({:receive_interrupt, :nmi}, _, state), do: {:reply, :ok, %{state | nmi: true}}
+  def handle_call({:receive_interrupt, :rst}, _, state), do: {:reply, :ok, %{state | rst: true}}
+
+  def handle_call({:set_state, new_state}, _, _), do: {:reply, :ok, new_state}
 
   # Private helpers
 
--- test/cpu_test.exs
+++ test/cpu_test.exs
@@ -5,6 +5,15 @@ defmodule Nintenlixir.CPUTest do
   alias Nintenlixir.CPU
   alias Nintenlixir.Memory
   alias Nintenlixir.Registers
+  alias Nintenlixir.ProcessorStatus
+
+  @initial_state %{
+    decimal_mode: true,
+    break_error: false,
+    nmi: false,
+    irq: false,
+    rst: false
+  }
 
   setup do
     start_supervised(CPU)
@@ -13,14 +22,8 @@ defmodule Nintenlixir.CPUTest do
     :ok
   end
 
-  test "CPU.get_state/0 should return a brand new CPU state in it's creation" do
-    assert %{
-             decimal_mode: true,
-             break_error: false,
-             nmi: false,
-             irq: false,
-             rst: false
-           } == CPU.get_state()
+  test "CPU.get_state/0" do
+    assert @initial_state == CPU.get_state()
   end
 
   test "CPU.reset/0" do
@@ -77,5 +80,108 @@ defmodule Nintenlixir.CPUTest do
     %{stack_pointer: new_sp} = Registers.get_registers(CPU.registers_server_name())
 
     assert new_sp == sp + 2
+  end
+
+  test "CPU.irq/0" do
+    assert :ok = CPU.irq()
+    assert %{@initial_state | irq: true} == CPU.get_state()
+  end
+
+  test "CPU.nmi/0" do
+    assert :ok = CPU.nmi()
+    assert %{@initial_state | nmi: true} == CPU.get_state()
+  end
+
+  test "CPU.rst/0" do
+    assert :ok = CPU.rst()
+    assert %{@initial_state | rst: true} == CPU.get_state()
+  end
+
+  test "CPU.interrupt/0 with no interrupts set" do
+    assert {:ok, 0} = CPU.interrupt()
+  end
+
+  test "CPU.interrupt/0 with irq" do
+    %{processor_status: p} = registers = Registers.get_registers(CPU.registers_server_name())
+
+    assert :ok =
+             Registers.set_registers(CPU.registers_server_name(), %{
+               registers
+               | processor_status: p &&& ~~~ProcessorStatus.InterruptDisable.value()
+             })
+
+    %{
+      program_counter: pc,
+      processor_status: p,
+      stack_pointer: sp
+    } = Registers.get_registers(CPU.registers_server_name())
+
+    {:ok, new_pc_low} = Memory.read(CPU.memory_server_name(), 0xFFFE)
+    {:ok, new_pc_high} = Memory.read(CPU.memory_server_name(), 0xFFFF)
+
+    assert :ok = CPU.irq()
+    assert %{irq: true} = CPU.get_state()
+    assert {:ok, 7} = CPU.interrupt()
+    assert %{irq: false} = CPU.get_state()
+
+    %{
+      program_counter: new_pc,
+      processor_status: new_p,
+      stack_pointer: new_sp
+    } = Registers.get_registers(CPU.registers_server_name())
+
+    {:ok, previous_p} = Memory.read(CPU.memory_server_name(), 0x0100 ||| sp - 2)
+    {:ok, previous_pc_low} = Memory.read(CPU.memory_server_name(), 0x0100 ||| sp - 1)
+    {:ok, previous_pc_high} = Memory.read(CPU.memory_server_name(), 0x0100 ||| sp)
+
+    assert ((previous_p ||| ProcessorStatus.Unused.value()) &&&
+              ~~~ProcessorStatus.BreakCommand.value()) == p
+
+    assert (previous_pc_high <<< 8 ||| previous_pc_low) == pc
+    assert (new_pc_high <<< 8 ||| new_pc_low) == new_pc
+    assert (p ||| ProcessorStatus.InterruptDisable.value()) == new_p
+    assert sp - 3 == new_sp
+  end
+
+  test "CPU.interrupt/0 with nmi" do
+    %{
+      program_counter: pc,
+      processor_status: p,
+      stack_pointer: sp
+    } = Registers.get_registers(CPU.registers_server_name())
+
+    {:ok, new_pc_low} = Memory.read(CPU.memory_server_name(), 0xFFFA)
+    {:ok, new_pc_high} = Memory.read(CPU.memory_server_name(), 0xFFFB)
+
+    assert :ok = CPU.nmi()
+    assert %{nmi: true} = CPU.get_state()
+    assert {:ok, 7} = CPU.interrupt()
+    assert %{nmi: false} = CPU.get_state()
+
+    %{
+      program_counter: new_pc,
+      processor_status: new_p,
+      stack_pointer: new_sp
+    } = Registers.get_registers(CPU.registers_server_name())
+
+    {:ok, previous_p} = Memory.read(CPU.memory_server_name(), 0x0100 ||| sp - 2)
+    {:ok, previous_pc_low} = Memory.read(CPU.memory_server_name(), 0x0100 ||| sp - 1)
+    {:ok, previous_pc_high} = Memory.read(CPU.memory_server_name(), 0x0100 ||| sp)
+
+    assert ((previous_p ||| ProcessorStatus.Unused.value()) &&&
+              ~~~ProcessorStatus.BreakCommand.value()) == p
+
+    assert (previous_pc_high <<< 8 ||| previous_pc_low) == pc
+    assert (new_pc_high <<< 8 ||| new_pc_low) == new_pc
+    assert (p ||| ProcessorStatus.InterruptDisable.value()) == new_p
+    assert sp - 3 == new_sp
+  end
+
+  test "CPU.interrupt/0 with rst" do
+    assert :ok = CPU.rst()
+    assert %{rst: true} = CPU.get_state()
+    assert {:ok, 7} = CPU.interrupt()
+    assert %{rst: false} = CPU.get_state()
+    assert %{program_counter: 0xFFFF} = Registers.get_registers(CPU.registers_server_name())
   end
 end
